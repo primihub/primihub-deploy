00220b331190f273b1d359955df680ca51ae998f
Unknown <unknown@example.com>
2024-04-08 02:40:01 +0800

    feat sql

--- docker-one-in-one/data/initsql/privacy.sql
+++ docker-one-in-one/data/initsql/privacy.sql
@@ -251,6 +251,22 @@ CREATE TABLE `data_pir_task` (
                                  PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = 'pir 任务表' ROW_FORMAT = DYNAMIC;
 
+DROP TABLE IF EXISTS `data_exam_task`;
+CREATE TABLE `data_exam_task`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `task_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '预处理任务id',
+  `task_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '任务名称',
+  `task_state` tinyint(4) NULL DEFAULT NULL COMMENT '任务状态(0未开始 1成功 2运行中 3失败 4取消)',
+  `origin_organ_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '源组织id',
+  `origin_resource_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '源资源id',
+  `target_organ_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '目标组织id',
+  `target_resource_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '目标资源id',
+  `is_del` tinyint(4) NULL DEFAULT 0 COMMENT '是否删除',
+  `create_date` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) COMMENT '创建时间',
+  `update_date` datetime(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3) COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '预处理任务表' ROW_FORMAT = DYNAMIC;
+
 DROP TABLE IF EXISTS `data_resource`;
 CREATE TABLE `data_resource`  (
                                   `resource_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '资源id',
